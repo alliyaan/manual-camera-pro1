c97daa9ab093a0f463306a2aaabf31cec4e1fbf9
ALi ANwar <alianwar776@hotmail.com>
2025-07-15 22:35:59 +0500

    Add files via upload

---
+++ android/src/main/kotlin/com/afonsoraposo/camera/ManualCameraProPlugin.kt
@@ -0,0 +1,24 @@
+package com.afonsoraposo.camera
+
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.MethodChannel
+
+class ManualCameraProPlugin : FlutterPlugin, ActivityAware {
+    private lateinit var channel: MethodChannel
+
+    override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(binding.binaryMessenger, "manual_camera_pro")
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {}
+    override fun onDetachedFromActivityForConfigChanges() {}
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
+    override fun onDetachedFromActivity() {}
+}
